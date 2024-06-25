c448a1318e58a627b68672a63a03991deb354669
SANJAY <sanjaypk986@gmail.com>
2024-06-25 17:09:36 +0530

    write all methods codes using schema and model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,38 @@
-const getAllUsers = async ( (req, res) => {
-    res.send("Hello World!");
-  });
-  const getUserById = async((req, res) => {
-    res.send("Hello World!");
-  });
-  const addUser = async((req, res) => {
-    res.send("Hello World!");
-  });
-  const updateUser = async( (req, res) => {
-    res.send("Hello World!");
-  });
-  const deleteUser= async( (req, res) => {
-    res.send("Hello World!");
-  });
+const User = require("../models/userModel");
+
+const getAllUsers = async (req, res) => {
+    const users = await User.find({});
+    res.json(users)
+  };
+  const getUserById = async(req, res) => {
+    try{
+        const user = await User.findById(req.params.userId).exec();
+        res.json(user);
+    }
+    catch(error){
+        res.status(500).json({ message: 'Error finding user', error });
+    }
+    
+  };
+  const addUser = (req, res) => {
+    const user = new User(req.body);
+    user.save();
+    res.json(user);
+  };
+  const updateUser = async (req, res) => {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(updatedUser);
+    } catch (error) {
+      res.status(500).json({ message: 'Error updating user', error });
+    }
+  };
+  const deleteUser= async(req, res) => {
+    const user = await User.findByIdAndDelete(req.params.userId)
+    res.send("user deleted");
+  };
 
   module.exports = {getAllUsers,getUserById,addUser,updateUser,deleteUser}
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
+const mongoose = require('mongoose');
 
 app.use(cors())
 app.use(express.json());
@@ -11,3 +13,11 @@ app.use('/users',userRoutes)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+main().then(() => console.log("Connected")).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.CONNECTION_STRING);
+
+
+}
